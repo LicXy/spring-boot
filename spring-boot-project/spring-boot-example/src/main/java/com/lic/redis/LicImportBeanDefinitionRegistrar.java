258c6ad98bca2ac56f7d4b4d443b6a01407198a5
lic <309846286@qq.com>
2020-11-18 17:43:08 +0800

    增加注释

--- spring-boot-project/spring-boot-example/src/main/java/com/lic/redis/LicImportBeanDefinitionRegistrar.java
+++ spring-boot-project/spring-boot-example/src/main/java/com/lic/redis/LicImportBeanDefinitionRegistrar.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
 
+/**
+ * 实现ImportBeanDefinitionRegistrar, 重写registerBeanDefinitions方法注册bean
+ */
 public class LicImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar {
+
 	@Override
 	public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
 		RootBeanDefinition beanDefinition = new RootBeanDefinition(LicServiceImpl3.class);
--- spring-boot-project/spring-boot-example/src/main/java/com/lic/redis/LicSelector.java
+++ spring-boot-project/spring-boot-example/src/main/java/com/lic/redis/LicSelector.java
@@ -5,7 +5,11 @@ import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.function.Predicate;
 
+/**
+ * 实现ImportSelector, 重写selectImports方法注册bean
+ */
 public class LicSelector implements ImportSelector {
+
 	@Override
 	public String[] selectImports(AnnotationMetadata annotationMetadata) {
 		return new String[]{"com.lic.redis.LicServiceImpl","com.lic.redis.LicServiceImpl2"};
